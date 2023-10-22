185d8499c1312089c7f2a00adaec5e1383da8909
Huseyn Huseynov <huseynovbabek1@gmail.com>
2023-10-22 22:54:20 +0400

    salam

---
+++ Whatsapp Design/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Whatsapp_Design.Models;
+namespace Whatsapp_Design;
+
+
+
+public partial class MainWindow : Window
+{
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+
+
+    }
+
+   
+}
---
+++ Whatsapp Design/MessageClass.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Whatsapp_Design.Models;
+
+public class MessageClass
+{
+
+    public string? Hour { get; set; }
+    public string? Text { get; set; }
+
+    public List<MessageClass> user1_messages { get; set; }
+    public List<MessageClass> user2_messages { get; set; }
+
+
+
+    public MessageClass()
+    {
+        user1_messages = new()
+        {
+            new MessageClass(){Hour = "10:13", Text = "Salam Vusal Necesen? " },
+            new MessageClass(){Hour = "10:16", Text = "Sagol mende yaxsiyam. Sabah derse geleceksen?" },
+        };
+
+        user2_messages = new()
+        {
+            new MessageClass(){Hour = "10:15", Text = "Salam Huseyn. Yaxsiyam sen necesen?" },
+            new MessageClass(){Hour = "10:18", Text = "Hee gelecem" },
+        };
+    }
+
+
+    public MessageClass(string? hour, string? text)
+    {
+        Hour = hour;
+        Text = text;
+    }
+
+
+
+}
